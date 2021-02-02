505ca538a81628d2363d83f55e1b7108d9b4eb92
Grahame Bowland <grahame@oreamnos.com.au>
2021-02-03 01:05:12 +0800

    verse display looking good

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,17 @@ import './App.css';
 import URLs from './urls.json';
 
 enum PlaybackRate {
+    VerySlow,
     Slow,
     Normal,
-    Fast
 };
 
-function Controls({displayInterlinear, setDisplayInterlinear}: {displayInterlinear: boolean, setDisplayInterlinear: any}) {
+type ControlsProps = {
+    displayInterlinear: boolean;
+    setDisplayInterlinear: any;
+};
+
+function Controls({ displayInterlinear, setDisplayInterlinear }: ControlsProps) {
     const [playbackRate, setPlaybackRate] = React.useState<PlaybackRate>(PlaybackRate.Normal);
     const audioRef = useRef<HTMLAudioElement>(null);
 
@@ -20,10 +25,10 @@ function Controls({displayInterlinear, setDisplayInterlinear}: {displayInterline
             return;
         }
         var rate: number = 1.0;
-        if (playbackRate === PlaybackRate.Slow) {
+        if (playbackRate === PlaybackRate.VerySlow) {
+            rate = 0.5;
+        } else if (playbackRate === PlaybackRate.Slow) {
             rate = 0.75;
-        } else if (playbackRate === PlaybackRate.Fast) {
-            rate = 1.25;
         }
         audioRef.current.playbackRate = rate;
     };
@@ -32,23 +37,31 @@ function Controls({displayInterlinear, setDisplayInterlinear}: {displayInterline
 
     function RateToggle({ value, label }: { value: PlaybackRate; label: string; }) {
         return <>
-                <ButtonToggle
-                    className={playbackRate === value ? 'active' : undefined}
-                    onClick={() => setPlaybackRate(value)}>{label}</ButtonToggle>
-            </>;
+            <ButtonToggle
+                className={playbackRate === value ? 'active' : undefined}
+                onClick={() => setPlaybackRate(value)}>{label}</ButtonToggle>
+        </>;
+    }
+
+    function InterlinearToggle() {
+        return <>
+            <ButtonToggle
+                className={displayInterlinear ? 'active' : undefined}
+                onClick={() => setDisplayInterlinear(!displayInterlinear)}>Interlinear</ButtonToggle>
+        </>;
     }
 
     return <>
         <div className="fixed-top bg-dark text-light">
             <Row>
-                <Col xs={{ size: 1, offset: 1 }}>
-                    <ButtonToggle>Interlinear</ButtonToggle>
+                <Col xs={{ size: 2, offset: 1 }}>
+                    <InterlinearToggle />
                 </Col>
-                <Col xs={{ size: 2 }}>
+                <Col xs={{ size: 3 }}>
                     <ButtonGroup>
+                        <RateToggle value={PlaybackRate.VerySlow} label="Very Slow" />
                         <RateToggle value={PlaybackRate.Slow} label="Slow" />
                         <RateToggle value={PlaybackRate.Normal} label="Normal" />
-                        <RateToggle value={PlaybackRate.Fast} label="Fast" />
                     </ButtonGroup>
                 </Col>
                 <Col xs={{ size: 2 }}>
@@ -69,13 +82,16 @@ function Controls({displayInterlinear, setDisplayInterlinear}: {displayInterline
 }
 
 function Footer() {
-    return <Row>
-        <p>
-            English and Hebrew text from the Bible/Tanakh is taken from <a target="_other" href="https://www.sefaria.org">Sefaria</a>.
+    return <>
+        <hr />
+        <Row>
+            <p>
+                English and Hebrew text from the Bible/Tanakh is taken from <a target="_other" href="https://www.sefaria.org">Sefaria</a>.
             Hebrew text is displayed using the <a target="_other" href="https://www.sbl-site.org/educational/BiblicalFonts_SBLHebrew.aspx">Society of Biblical Literature Hebrew font</a>.
             Recordings were downloaded from <a href="https://archive.org/">archive.org</a> - they were entrusted to the Carmelites. <a target="_other" href="http://individual.utoronto.ca/mfkolarcik/AbrahamShmuelof.html">More about Abraham Shmuelof.</a>
-        </p>
-    </Row>;
+            </p>
+        </Row>
+    </>;
 }
 
 function App() {
@@ -87,11 +103,10 @@ function App() {
             <Controls displayInterlinear={displayInterlinear} setDisplayInterlinear={setDisplayInterlinear} />
             <Container id="main">
                 <Row className="mb-4 mt-4">
-                    <Col xs={{ size: 10, offset: 1 }} className="text-right">
+                    <Col xs={{ size: 10, offset: 1 }}>
                         <Sefaria displayInterlinear={displayInterlinear} verse="Genesis 8"></Sefaria>
                     </Col>
                 </Row>
-                <hr></hr>
                 <Footer />
             </Container>
         </div>
--- src/Sefaria.tsx
+++ src/Sefaria.tsx
@@ -1,6 +1,54 @@
 import React from 'react';
 import axios from 'axios';
-export {Sefaria};
+import { Row, Col } from 'reactstrap';
+
+// this isn't complete, it's just a sketch of what we get back
+// full docs at https://github.com/Sefaria/Sefaria-Project/wiki/API-Documentation#text-api
+type SefariaResponse = {
+    book: string,
+    categories: [string],
+    commentary: [string],
+    he: [string],
+    next: string,
+    order: [number],
+    prev: string,
+    ref: string,
+    sectionNames: [string],
+    sections: [number],
+    text: [string],
+    titleVariants: [string],
+    toSections: [number],
+    type: string,
+    versionSource: string,
+    versionTitle: string,
+    versions: [string] | undefined,
+};
+
+type VerseProps = {
+    interlinear: boolean,
+    number: number,
+    hebrew: string,
+    english: string,
+};
+
+
+const Verse: React.FunctionComponent<VerseProps> = ({ number, hebrew, english, interlinear }: VerseProps) => {
+    var englishElem;
+    if (interlinear) {
+        englishElem = <div className="english interlinear text-left">{ english }</div>
+    }
+    return <>
+        <Row className="verse-row">
+            <Col xs={{ size: 10, offset: 1 }}>
+                <div className="biblical-hebrew text-right">{ hebrew }</div>
+                { englishElem }
+            </Col>
+            <Col xs={{ size: 1}}>
+                { number }
+            </Col>
+        </Row>
+    </>;
+}
 
 type SefariaProps = {
     verse: string,
@@ -8,7 +56,8 @@ type SefariaProps = {
 };
 
 const Sefaria: React.FunctionComponent<SefariaProps> = ({ verse, displayInterlinear }: SefariaProps) => {
-    const [sefariaResponse, setSefariaResponse]: [any, any] = React.useState({ 'text': '', 'he': '' });
+    const [sefariaResponse, setSefariaResponse] = React.useState<SefariaResponse>();
+    const [sefariaError, setSefariaError]: [string|undefined, any] = React.useState();
 
     React.useEffect(() => {
         axios
@@ -19,11 +68,32 @@ const Sefaria: React.FunctionComponent<SefariaProps> = ({ verse, displayInterlin
                 timeout: 5000,
             })
             .then((response) => {
+                setSefariaError();
                 setSefariaResponse(response.data);
             })
             .catch((ex) => {
+                setSefariaError(`Unable to retrieve scripture from Sefaria ${ex.response.status}`);
             });
     }, [verse]);
 
-    return <div className="biblical-hebrew display-3">{sefariaResponse.he}</div>;
+    if (sefariaError) {
+        return <div>{sefariaError}</div>;
+    }
+
+    if (!sefariaResponse) {
+        return <div>Retrieving scripture...</div>;
+    }
+
+    const verses = sefariaResponse.he.map((hebrew, idx) => 
+        <Verse interlinear={displayInterlinear} number={idx+1} hebrew={hebrew} english={sefariaResponse.text[idx]} />
+    );
+
+    return <>
+        { verses }
+        <p>
+            English translation: <a target="_other" href={ sefariaResponse.versionSource }>{ sefariaResponse.versionTitle }</a>
+        </p>
+    </>;
 };
+
+export {Sefaria};
