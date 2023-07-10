c9a8a59981456de14cfc6ce8cb91f19ace4ded73
Grahame Bowland <grahame@bowland.au>
2023-07-10 22:31:24 +0800

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import "core-js/es/object/assign";
 import "core-js/es/string/replace-all";
 import "raf/polyfill";
 import React from "react";
-import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
